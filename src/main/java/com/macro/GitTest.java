37717404837c58a38b452263fe17686bc01a0a00
macro <1257048897@qq.com>
2023-08-25 11:09:29 +0800

    second commit

--- src/main/java/com/macro/GitTest.java
+++ src/main/java/com/macro/GitTest.java
@@ -3,5 +3,6 @@ package com.macro;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello git");
     }
 }
